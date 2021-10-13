f2b977509ac2f09ee46f1d1644739a6a56444558
PriyanKishoreMS <priyankishore2000@gmail.com>
2021-10-13 17:12:36 +0530

    countdown

--- public/js/index.js
+++ public/js/index.js
@@ -9,3 +9,30 @@ function toggleMenu() {
   menuToggle.classList.toggle('active');
   menu.classList.toggle('active');
 }
+
+var countDate = new Date("October 27, 2021 00:00:00").getTime()
+
+const oasysDay = () => {
+  var now = new Date().getTime()
+  var gap = countDate - now
+
+  var second = 1000
+  var minute = second * 60
+  var hour = minute * 60
+  var day = hour * 24
+
+  var d = Math.floor(gap / (day))
+  var h = Math.floor((gap % (day)) / hour)
+  var m = Math.floor((gap % (hour) / minute))
+  var s = Math.floor((gap % minute) / second)
+
+  document.getElementById("days").innerText = d
+  document.getElementById("hours").innerText = h
+  document.getElementById("mins").innerText = m
+  document.getElementById("secs").innerText = s
+
+}
+
+setInterval(() => {
+  oasysDay();
+},1000)
